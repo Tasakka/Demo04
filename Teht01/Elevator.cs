e39a2c5cdf46c7c582cf8fd3ed2c2108211fbe1e
k3236 <k3236@student.jamk.fi>
2017-02-08 14:25:28 +0200

    fhgj

---
+++ Teht01/Elevator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht01
+{
+    class Elevator
+    {
+        private int minfloor = 1;
+        private int maxfloor = 5;
+        private int floor = 1;
+
+        public int Floor
+        {
+            get
+            {
+                return floor;
+            }
+            set
+            {
+                if (value >= minfloor && value <= maxfloor)
+                {
+                    floor = value;
+                }
+                else if (value < 1)
+                {
+                    Console.WriteLine("Floor is too small!!");
+                }
+                else if (value > 5)
+                {
+                    Console.WriteLine("Floor is too big!!");
+                }
+            }
+        }
+    }
+}
--- Teht01/Program.cs
+++ Teht01/Program.cs
@@ -10,6 +10,18 @@ namespace Teht01
     {
         static void Main(string[] args)
         {
+            Elevator elevator = new Elevator();
+            Console.WriteLine("Elevator is in now in floor: " + elevator.Floor);
+            bool result;
+            do
+            {
+                Console.Write("Give a new floor number (1-5) > ");
+                string str = Console.ReadLine();
+                int num;
+                result = int.TryParse(str, out num);
+                elevator.Floor = num;
+                Console.WriteLine("Elevator is now in floor: " + elevator.Floor);
+            } while (result == true);
         }
     }
 }
