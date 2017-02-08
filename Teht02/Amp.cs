ee68f0e6fba2098cfdef10b509bd9a96931e7a82
k3236 <k3236@student.jamk.fi>
2017-02-08 15:11:04 +0200

    asdf

---
+++ Teht02/Amp.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht02
+{
+    class Amp
+    {
+        private int volume = 0;
+        private int maxvolume = 100;
+        private int minvolume = 0;
+        
+        public int Volume
+        {
+            get
+            {
+                return volume;
+            }
+            set
+            {
+                if (value >= minvolume && value <= maxvolume)
+                {
+                    volume = value;
+                }
+                    if (volume == maxvolume)
+                {
+                    Console.WriteLine("Volume is se to max");
+                }
+                    if (volume == minvolume)
+                {
+                    Console.WriteLine("Volume is set to min");
+                }
+                else if (value > maxvolume)
+                {
+                    Console.WriteLine("CAN'T GO ANY HIGHER!");
+                    volume = maxvolume;
+                }
+                else if (value < minvolume)
+                {
+                    Console.WriteLine("Can't go any lower.");
+                    volume = minvolume;
+                }
+            }
+        }
+    }
+}
---
+++ Teht02/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Amp amp = new Amp();
+            Console.WriteLine("The starting volume is " + amp.Volume);
+            bool result;
+            do
+            {
+                Console.Write("Give a new volume > ");
+                string str = Console.ReadLine();
+                int num;
+                result = int.TryParse(str, out num);
+                amp.Volume = num;
+                Console.WriteLine("The volume is: " + amp.Volume);
+            } while (result == true);
+        }
+    }
+}
